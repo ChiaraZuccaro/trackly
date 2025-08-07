9114677652c7f25bf56a2eb2a0e5d475f7582999
Chiara <chiara-zuc@libero.it>
2025-08-07 23:29:48 +0200

    origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 
 const PORT = 3000;
@@ -7,8 +8,13 @@ const baseUrl = "/trackly_api";
 const app = express();
 
 dotenv.config();
+const validUrl = 'http://localhost:1008';
 
 app.use(express.json());
+app.use(cors({
+  origin: validUrl,
+  credentials: true,
+}));
 app.use(baseUrl + '/auth', authRoutes)
 
 app.listen(PORT, () => { console.log('init trackly'); })
